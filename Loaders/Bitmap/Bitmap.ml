7963b921fc94df0f705dce889ed19599e18dba1e
Jupotter <jupotter.g@gmail.com>
2010-10-09 15:44:12 +0200

    Retire l'executable

--- Loaders/Bitmap/Bitmap.ml
+++ Loaders/Bitmap/Bitmap.ml
@@ -1,14 +1,29 @@
 open BasicTypes
+
+
 (* This is launch at the beginning *)
+
 let initialize () = ();;
+
+
 (* Save an image at the specified location *)
+
 let save_image_bmp (i:image) (s:string) = ();;
+
+
 (* Open a bitmap file and return the image *)
+
 let load_image_bmp (s:string) = 
 	let file = new file_in s in
 	file#read
 ;;
+
+
 (* Save an image at the specified location *)
+
 let save_image_png (i:image) (s:string) = ();;
+
+
 (* Open a Portable Network Graphics file and return the image *)
+
 let load_image_png (s:string) = open_in s;;
