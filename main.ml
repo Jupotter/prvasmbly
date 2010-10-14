771bfbf704e602f9bb60e59a0c40be138d618af3
Jupotter <jupotter.g@gmail.com>
2010-10-14 21:27:26 +0200

    Ajout du type bmp_header

--- main.ml
+++ main.ml
@@ -2,6 +2,7 @@ let main =
 	Bitmap.initialize();
 	Engine.initialize();
 	Analyzer.initialize();
-	Interface.initialize()
-		
+	Interface.initialize();
+	Bitmap.load_image_bmp "/home/Jjpotter/prvasmbly/img_test.bmp"	
+
 let _ = main
