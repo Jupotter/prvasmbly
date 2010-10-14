576ccd50bc4fd864e576b71fb7651e76ca769e9c
Jupotter <jupotter.g@gmail.com>
2010-10-14 22:46:18 +0200

    Saver: ecris le header

--- Loaders/Bitmap/Bitmap.ml
+++ Loaders/Bitmap/Bitmap.ml
@@ -8,11 +8,69 @@ let initialize () = ();;
 
 (* Save an image at the specified location *)
 
-let save_image_bmp (i:image) (s:string) = 
-	let
-;;
+let save_image_bmp (i:image) (s:string) =
+	let f = new file_out s in
+	let (h,w) = (i#height,i#width) in
+	let size = h * w * 4 + 54 in
+	let _ =
+	begin
+	f#write_byte 66;
+	f#write_byte 77;
+	f#write_byte (size mod 256);
+	f#write_byte ((size mod 256) /256);
+	f#write_byte ((size mod 256*256) /(256*256));
+	f#write_byte (size / (256*256*256));
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 54;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 40;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte (w mod 256);
+	f#write_byte ((w mod 256) /256);
+	f#write_byte ((w mod 256*256) /(256*256));
+	f#write_byte (w / (256*256*256));
+	f#write_byte (h mod 256);
+	f#write_byte ((h mod 256) /256);
+	f#write_byte ((h mod 256*256) /(256*256));
+	f#write_byte (h / (256*256*256));
+	f#write_byte 1;
+	f#write_byte 0;
+	f#write_byte 32;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte ((size mod 256)-54);
+	f#write_byte ((size mod 256) /256);
+	f#write_byte ((size mod 256*256) /(256*256));
+	f#write_byte (size / (256*256*256));
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	f#write_byte 0;
+	end in
+	();;
 
-(*tuple used to store the header of a bmp file*)
 
 type bmp_header = {
         fsize : int;
@@ -24,7 +82,7 @@ type bmp_header = {
 	isize : int;
         mapsize : int}
 
-let load_bmp_header (f:file_in) = 
+let load_bmp_header (f:file_in) =
         let size = f#read_int in
         let _ = f#read_int in
         let startpos = f #read_int in
@@ -37,7 +95,7 @@ let load_bmp_header (f:file_in) =
         let _ = f#read_n_byte 8 in
         let mapsize = f#read_int in
         let _ = f#read_n_byte 4 in
-        let header = 
+        let header =
 		{fsize = size;
 		startpos = startpos;
                 width = width;
@@ -45,17 +103,17 @@ let load_bmp_header (f:file_in) =
                 depth = depth;
 		compress = compress;
 		isize = isize;
-		mapsize = mapsize;} 
+		mapsize = mapsize;}
 		in header;;
 
 
 (* Open a bitmap file and return the image *)
-let read_bmp_32 (f:file_in) head = 
+let read_bmp_32 (f:file_in) head =
 	let (h,w,p) =  (head.height,
 			head.width,
 			head.startpos) in
         let image = new image h w in
-        let _ = f#seek_pos p in 
+        let _ = f#seek_pos p in
         let _ = print_int (f#get_pos ) in
         let _ = for i = h-1 downto 0 do
                 for j = 0 to w-1 do
@@ -89,7 +147,7 @@ let read_bmp_24 (f:file_in) head =
                 done;
 		let max = if (w/4*4 = w)
 			then w-1
-			else ((3*w/4+1)*4)-1 
+			else ((3*w/4+1)*4)-1
 			in
                 for j = w*3 to max do
                         let _ = f#read_byte in
@@ -99,7 +157,7 @@ let read_bmp_24 (f:file_in) head =
         done
         in image;;
 
-let load_image_bmp (s:string) = 
+let load_image_bmp (s:string) =
 	let file = new file_in s in
         let ident = file#read_n_char 2 in
         match ident with
@@ -118,19 +176,19 @@ let load_image_bmp (s:string) =
                 let _ = file#read_n_byte 4 in*)
                 (*let _ = print_int depth ;print_newline in*)
 		let head = load_bmp_header file in
-                begin 
-                match head.depth with 
+                begin
+                match head.depth with
                 |24 -> read_bmp_24 file head
                 |32 -> read_bmp_32 file head
                 |_ -> raise (Invalid_argument "Unimplemented Color Depth")
                 end
         |_->
                 raise (Invalid_argument (s));;
-                
+
 
 (* Save an image at the specified location *)
 
-let save_image_png (i:image) (s:string) = ();; 
+let save_image_png (i:image) (s:string) = ();;
 
 
 (* Open a Portable Network Graphics file and return the image *)
