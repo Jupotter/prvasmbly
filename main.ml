a1f144252b9bea1461719b91b47d35904ddada9c
Raphael Boissel <Raphael@raphael-pc.(none)>
2010-10-21 21:06:53 +0200

    modification des moteurs et optimisation

--- main.ml
+++ main.ml
@@ -4,35 +4,35 @@ let sdl_init () =
     Sdlevent.enable_events Sdlevent.all_events_mask;
   end
 
-let main() =
+let main() = 
 	sdl_init();
+	
+	let win = new Engine.window in
+	let msh = Engine.create_grild 502 502 20 in
+	let flcl = new BasicTypes.image 1 1 in
+	flcl#load_file "hm.jpg";
 
-(*	let win = new Engine.window in
-	let msh = Engine.create_grild 512 512 10.0 in
 	let hmap = new BasicTypes.image 1 1 in
 	hmap#load_file "carte.bmp";
+
 	let img = new BasicTypes.image 1 1 in
 	img#load_file "carte.bmp";
-	let outimg = new BasicTypes.image img#width img#height
-	Analyzer.outlines i outimg
-	double_diff#save_file "outline.bmp"
+
+
+
+	let sprt = new Engine.sprite (hmap#clone) in
 	msh#apply_color (img);
 	msh#apply_height (hmap);
 	msh#save "hello.obj";
 	win#add_mesh msh;
+	win#add_sprite sprt;
+	sprt#set_on_click (fun _-> sprt#move(0.1,0.1););
 	while true do
 		win#events;
 		win#draw;
+		win#update;
 	done
-*)
-	let map = new BasicTypes.image 1 1 in
-	map#load_file "carte.bmp";
-	let outmap = map in
-		let _ = Analyzer.double_diff outmap in
-		Bitmap.save_image_bmp outmap "outmap.bmp";
-	let outmap = map in
-		let _ = Analyzer.outlines map outmap in
-		Bitmap.save_image_bmp outmap "outmapB.bmp"
+
 
 let _ = main();
 
