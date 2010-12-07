b7cae335d77a44adab835a40abc944311dd7395c
Jupotter <jupotter.g@gmail.com>
2010-12-07 22:09:18 +0100

    C'est noir! Prise 2

--- Gtk/Gtkint.ml
+++ Gtk/Gtkint.ml
@@ -1,24 +1,24 @@
-let engine_load_map file = 
+let engine_load_map file =
 	print_string ("chargement du fichier : " ^ file); flush stdout(* FIX ME : Recharger une map *)
 
 
 
-let gtk_open_bitmap (parent_window) = 
+let gtk_open_bitmap (parent_window) =
 
 	let filesel = GWindow.file_selection
-	~title:"Open File" 
-	~show_fileops:true 
-	~select_multiple:false 
-	~parent:parent_window 
-	~destroy_with_parent:true 
-	~allow_grow:false 
+	~title:"Open File"
+	~show_fileops:true
+	~select_multiple:false
+	~parent:parent_window
+	~destroy_with_parent:true
+	~allow_grow:false
 	~allow_shrink:false
 	~resizable:false () in
 
 
 		let destroy () = () in
 		let destroyf () = filesel#destroy () in (* destruction forcée *)
-		let load () =     engine_load_map filesel#filename ; filesel#destroy () in 
+		let load () =     engine_load_map filesel#filename ; filesel#destroy () in
 		(* pas sur que opengl accepte de se faire flushé avec callback a verifier  au pire virer le callback*)
 
 
@@ -30,10 +30,17 @@ let gtk_open_bitmap (parent_window) =
 
 
 let destroy () = GMain.Main.quit ()
-let refresh3D () = () (* FIX ME : Appeller le display de la fenetre 3D *)
 
-let right_bar () = 
+let refresh3D () =
+	let window = new Engine.display in
+	window#draw
+
+let right_bar () =
 	let vpaned = GPack.paned `VERTICAL
+			~border_width:4
+			~height:200
+			~width:200() in
+	let hpaned = GPack.paned `HORIZONTAL
 			~border_width:4
 			~height:200
 			~width:200() in
@@ -41,7 +48,7 @@ let right_bar () =
 			~hpolicy: `AUTOMATIC ~vpolicy: `AUTOMATIC in
 		let list_view = GTree.view () ~packing:sw#add in
 		vpaned#coerce
-	
+
 let gtk_init () =
 	let _ = GMain.init () in
 	let window = GWindow.window ~width:800 ~height:480 () in
@@ -53,7 +60,7 @@ let gtk_init () =
 				~style:`BOTH
 				~border_width:0
 				~height:35
-				~packing:vpaned#add1() in 
+				~packing:vpaned#add1() in
 				(*ajout dans la partie haute du VPanel*)
 				(*FIX ME : Initialisation des boutons *)
 				(* NOTE : Le warning disparaitra apres initialisation *)
@@ -65,13 +72,13 @@ let gtk_init () =
 					 ~height:400
 					 ~packing:hpaned#add1 ()
 					 in
-		
+
 		let _ = openGLArea#connect#display ~callback:refresh3D in
-		
 
 		gtk_open_bitmap window;
-		window#show ();	
-		openGLArea#make_current();		
+		window#show ();
+		openGLArea#make_current();
+		openGLArea#swap_buffers ();
 		GMain.Main.main ();
   	end
 
